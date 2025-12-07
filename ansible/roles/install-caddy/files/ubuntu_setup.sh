02a70568abd799b3a45a3010566794fc75261a1a
philmish <philmish.dev@gmail.com>
2025-12-07 21:36:48 +0100

    feat: add role for installing caddy on ubuntu

---
+++ ansible/roles/install-caddy/files/ubuntu_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xe
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
