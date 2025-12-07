c26aa5b27977ecb1ba354a75dcfccd049b9b9284
philmish <philmish.dev@gmail.com>
2025-12-07 20:00:33 +0100

    feat: add role for installing redis on ubuntu

---
+++ ansible/roles/install-redis/files/ubuntu_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# debug output enabled
+set -xe
+
+curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list
+
+
