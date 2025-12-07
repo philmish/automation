683b1ae39d1dc3a93495b0e9057c6c781fc1b1f4
philmish <philmish.dev@gmail.com>
2025-12-07 18:02:50 +0100

    feat: add role for installing docker on ubuntu nodes

---
+++ ansible/roles/install-docker/files/ubuntu_setup.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# taken from dockers offical docs: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+#
+# slightly modified to execute as much with ansible as possible
+
+set -xe
+
+# Add Docker's official GPG key:
+#
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
